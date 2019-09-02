f6052be677c8fb1388b3a89374464d0ad11b1855
Sebastian Roth <sebastian@nextbase.co.uk>
2019-09-02 18:31:04 +0100

    Adds toString/hashCode to AmazonUser method.

--- lib/src/model/amazon_user.dart
+++ lib/src/model/amazon_user.dart
@@ -8,5 +8,23 @@ class AmazonUser {
 
   final String email;
   final String userId;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is AmazonUser &&
+              runtimeType == other.runtimeType &&
+              email == other.email &&
+              userId == other.userId;
+
+  @override
+  int get hashCode =>
+      email.hashCode ^
+      userId.hashCode;
+
+  @override
+  String toString() {
+    return 'AmazonUser{email: $email, userId: $userId}';
+  }
 }
 
