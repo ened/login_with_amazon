a320eecd31fe8d2870958bc55b935e25fd346775
Sebastian Roth <sebastian@nextbase.co.uk>
2019-11-18 11:18:37 +0000

    bump flutter / e2e plugins

--- example/test/login_with_amazon_cloud_test.dart
+++ example/test/login_with_amazon_cloud_test.dart
@@ -1,9 +1,9 @@
+import 'package:e2e/e2e.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:instrumentation_adapter/instrumentation_adapter.dart';
 import 'package:login_with_amazon/login_with_amazon.dart';
 
 void main() {
-  InstrumentationAdapterFlutterBinding.ensureInitialized();
+  E2EWidgetsFlutterBinding.ensureInitialized();
 
   test('Call SDK version', () async {
     expect(await LoginWithAmazon().getSdkVersion(), '3.0.6');
